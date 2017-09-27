487b1330c5c645d457f1b720e72a58776b598117
andyfiveeleven <fouracesguitar@gmail.com>
2017-09-27 15:31:41 -0700

    user model finished for now

--- model/user.js
+++ model/user.js
@@ -14,4 +14,49 @@ const userSchema = Schema({
   email: { type: String, required:true, unique: true},
   password: { type: String, required: true},
   findHash: {type: String, unique: true},
+  // admin: {type: Boolean, default: false}
 });
+
+userSchema.methods.generatePasswordHash = function(password){
+  debug('generatePasswordHash');
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(password, 10, (err,hash) => {
+      if(err) return reject(err);
+      this.password = hash;
+      resolve(this);
+    });
+  });
+};
+
+userSchema.methods.comparePasswordHash = function(password){
+  debug('comparePasswordHash');
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, this.password, (err, valid) => {
+      if (err) return reject(err);
+      if (!valid) return reject(createError(401, 'unauthorized'));
+      resolve(this);
+    });
+  });
+};
+
+userSchema.methods.generateFindHash = function() {
+  debug('generateFindHash');
+
+  return new Promise((resolve, reject) => {
+    this.findHash = crypto.randomBytes(32).toString('hex');
+    this.save()
+    .then(() => resolve(this.findHash))
+    .catch((err) => reject(err));
+  });
+};
+
+userSchema.methods.generateToken = function() {
+  debug('generateToken');
+
+  return new Promise((resolve, reject) => {
+    this.generateFindHash()
+    .then((findHash) => resolve(jwt.sign({token: findHash}, process.env.APP_SECRET)))
+    .catch((err) => reject(err));
+  });
+};
